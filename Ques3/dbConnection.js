71e1b5a3358bc4ac53dd87a7517ef93a7b7eda23
Sumit Sharma <sumitjmrss1501@gmail.com>
2024-09-07 00:10:38 +0530

    Some Changes

--- Ques3/dbConnection.js
+++ Ques3/dbConnection.js
@@ -1,1 +1,14 @@
-const {MongoClient} = require('mongodb');
+const mongoose = require ('mongoose');
+const { log } = require('winston');
+
+// Connection with mongodb database
+
+mongoose.connect('mongodb://127.0.0.1:27017/myWork')
+.then(()=>{
+    console.log("Connection Successful");
+})
+.catch((err)=>{
+    console.log("Error Occured :" + err);
+});
+
+// 
